ff10fcbb8ae42319e5811ce5383beb9e51442846
tankeiei <tankun.p@kkumail.com>
2026-08-12 19:36:27 +0700

    Restructure project files and move templates to resources

---
+++ src/main/java/com/example/lab7_673380301_0_sec1/Lab76733803010Sec1Application.java
@@ -0,0 +1,13 @@
+package com.example.lab7_673380301_0_sec1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab76733803010Sec1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab76733803010Sec1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab7_673380301_0_sec1/Lab76733803010Sec1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7_673380301_0_sec1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab76733803010Sec1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
